1d8b7eaa47d356464dd2c095143f171414e03613
Hieu Le <minhhieuzz1603@gmail.com>
2022-07-31 16:51:59 +0700

    completed 60% Home Page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,11 @@
 import styles from './Home.module.scss';
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faArrowRotateRight,
+    faCreditCard,
+    faPlaneUp,
+} from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
@@ -27,6 +33,7 @@ function Home() {
                 </div>
             </div>
             <div className={cx('breadcrums')}></div>
+
             <div className={cx('container-2')}>
                 <div className={cx('container2-left')}>
                     <img
@@ -47,6 +54,80 @@ function Home() {
                         <button className={cx('button-container')}>
                             SHOP NOW
                         </button>
+                    </div>
+                </div>
+            </div>
+            <div className={cx('breadcrums')}></div>
+            <div className={cx('wrapper-slider')}>
+                <div className={cx('container-slider')}>
+                    <div className={cx('slider-title')}>
+                        <h3> On sale </h3>
+                    </div>
+                </div>
+            </div>
+            <div className={cx('breadcrums')}></div>
+            <div className={cx('container-3')}>
+                <div className={cx('container2-left')}>
+                    <img
+                        className={cx('container2-img')}
+                        src="https://cdn.shopify.com/s/files/1/0453/5035/5103/files/img_06_900x.png?v=1644319132"
+                        alt="anh container 3"
+                    />
+                </div>
+                <div className={cx('container2-right')}>
+                    <div className={cx('container2-left-wrapper')}>
+                        <h6 className={cx('container-h6')}>
+                            EXCLUSIVE IPHONE ACCESSORIES
+                        </h6>
+                        <h3 className={cx('container-h3')}>
+                            Get up to 20% off
+                        </h3>
+                        <p className={cx('container-p')}>The latest trends</p>
+                        <button className={cx('button-container')}>
+                            SHOP NOW
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className={cx('breadcrums')}></div>
+            <div className={cx('sections-wrapper')}>
+                <div className={cx('sections')}>
+                    <div className={cx('sections-item')}>
+                        <FontAwesomeIcon
+                            icon={faPlaneUp}
+                            className={cx('sections-icon')}
+                        />
+                        <h3 className={cx('sections-title')}>FREE SHIPPING</h3>
+                        <p className={cx('sections-decs')}>
+                            {' '}
+                            On all orders over $50.00
+                        </p>
+                    </div>
+                    <div className={cx('sections-item')}>
+                        <FontAwesomeIcon
+                            icon={faCreditCard}
+                            className={cx('sections-icon')}
+                        />
+                        <h3 className={cx('sections-title')}>
+                            100% PAYMENT SECURE
+                        </h3>
+                        <p className={cx('sections-decs')}>
+                            {' '}
+                            We ensure secure payment with PEV
+                        </p>
+                    </div>
+                    <div className={cx('sections-item')}>
+                        <FontAwesomeIcon
+                            icon={faArrowRotateRight}
+                            className={cx('sections-icon')}
+                        />
+                        <h3 className={cx('sections-title')}>
+                            30 DAYS GUARANTEE
+                        </h3>
+                        <p className={cx('sections-decs')}>
+                            {' '}
+                            30-days free return policy
+                        </p>
                     </div>
                 </div>
             </div>
